7406aad8bfebf342e6e624cbcfa48a97c7b816ef
Carl Louis Rodriguez (carlrodr) <carl.rodriguez@unc.edu>
2024-12-13 13:36:10 -0500

    updated submodules and logic flow

--- include/bse_wrap/bse_wrap.h
+++ include/bse_wrap/bse_wrap.h
@@ -197,7 +197,7 @@ extern struct { double dmmax, drmax; } tstepc_;
 extern struct { double scm[14][50000], spp[3][20]; } single_;
 extern struct { double bcm[38][50000], bpp[43][1000]; } binary_;
 extern struct { double merger; long int id1_pass, id2_pass; long int using_cmc; } cmcpass_;
-extern struct { int using_sse; int using_metisse; } se_flags_;
+extern struct { int using_metisse; int using_sse; } se_flags_;
 
 /* setters */
 void bse_set_idum(int idum); /* RNG seed (for NS birth kicks) */
